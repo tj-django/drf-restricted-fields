6377cec4857501815701d2f6e2ff4fe4804829a2
Tonye Jack <jtonye@ymail.com>
2021-03-03 23:18:05 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import drf_restricted_fields
+import restricted_fields
 
 # -- General configuration ---------------------------------------------
 
