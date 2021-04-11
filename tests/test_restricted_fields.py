39c5d50839c8853697b00e108f02f4a519cfa42a
Tonye Jack <jtonye@ymail.com>
2021-04-11 00:28:30 -0400

    Updated test.

--- tests/test_restricted_fields.py
+++ tests/test_restricted_fields.py
@@ -8,17 +8,17 @@ import pytest
 from restricted_fields import *
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
+#
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-
-def test_content(response):
+def test_content():
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
+    assert 1 in [2, 3, 1]
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
