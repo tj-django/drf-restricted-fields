f9374f9e24ca05f9c65cb7fcd356725b820e0269
Tonye Jack <jtonye@ymail.com>
2021-04-10 22:43:27 -0400

    Fixed long description content type.

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
     install_requires=install_requires,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type="text/x-rst",
     include_package_data=True,
     keywords=['drf_restricted_fields', 'restricted_fields'],
     name='drf_restricted_fields',
